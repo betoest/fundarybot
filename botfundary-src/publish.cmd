b8a2bfcfc9f84e1295762380e2c2a3981782ca95
Roberto Jesús Estrada Benavides <roberto.estrada@fundary.com>
2019-03-27 10:34:46 -0600

    Primer Commit
    FundaryBot First Commit

---
+++ botfundary-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=botfundary-Web-Deploy.pubxml -p:Password=3dZyk6tG21u5sm8xlRziER82mNoGKhrYMJCF9kwWlv0WTjolHn3PRuyHFQy6
+
